63136a373a693a9724b4a7bda9ff53d6636e7606
luke tebo <77975195+luketebo@users.noreply.github.com>
2022-04-22 23:16:57 +0800

    submit demo

---
+++ Java_Study_For_School_projects/Springboot/spring/home_0422/src/main/java/com/luketebo/Home0422Application.java
@@ -0,0 +1,13 @@
+package com.luketebo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home0422Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Home0422Application.class, args);
+    }
+
+}
--- Java_Study_For_School_projects/Springboot/spring/springboot_01_ssmp/src/main/java/com/luketebo/controller/BookController.java
+++ Java_Study_For_School_projects/Springboot/spring/springboot_01_ssmp/src/main/java/com/luketebo/controller/BookController.java
@@ -1,4 +1,42 @@
 package com.luketebo.controller;
 
+import com.luketebo.domain.Book;
+import com.luketebo.service.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+    @Autowired
+    private IBookService iBookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return iBookService.list();
+    }
+
+    @PostMapping
+    public Boolean save(Book book){
+        return iBookService.save(book);
+    }
+
+    @PutMapping()
+    public Boolean update(@PathVariable Book book){
+        return iBookService.update(book,null);
+    }
+
+    @DeleteMapping("{id}")
+    public Boolean delete(@PathVariable String id){
+        return iBookService.removeById(id);
+    }
+
+    @GetMapping("{id}")
+    public Book getById(@PathVariable String id){
+        return iBookService.getById(id);
+    }
+
 }
+
